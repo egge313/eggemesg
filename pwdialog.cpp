ef7ef1986bfc8d006b621a0173b353422b998e14
Esa Kettunen <esa.kettunen@gmx.com>
2019-06-29 18:28:52 +0300

    Added new tab 'Precaution' into the password dialog.

--- pwdialog.cpp
+++ pwdialog.cpp
@@ -8,64 +8,86 @@ PwDialog::PwDialog(QWidget *parent) :
     ui(new Ui::PwDialog)
 {
     ui->setupUi(this);
-    // ui->pushButtonDummy->hide();
-    //    connect(ui->buttonBox, SIGNAL(clicked()),
-    //        this, SLOT(onButtonClick));
-    // connect(ui->buttonBox, &QDialogButtonBox::accepted, this, SLOT(accept()));    
-    //    connect(ui->buttonBox, &QDialogButtonBox::accepted, this, SLOT(accept));
-    // connect(ui->buttonBox, &QDialogButtonBox::rejected, this, SLOT(reject));
 }
 
 void PwDialog::accept ()
 {
   switch (ui->tabWidget->currentIndex())
     {
-    case 0:
+    case 0: /* login tab */
       if (ui->lineEditUser1->text().length() > 0 &&
-	  ui->lineEditPw1->text().length() > 0)
-	{
-	  m_userdata = new UserData(ui->lineEditUser1->text(),
-				  ui->lineEditPw1->text(),
-				  false);
-	  QDialog::accept();
-	}
+          ui->lineEditPw1->text().length() > 0)
+        {
+          m_userdata = new UserData(ui->lineEditUser1->text(),
+                                  ui->lineEditPw1->text(),
+                                    "",
+                                  false);
+          QDialog::accept();
+        }
       else
-	{
-	  QMessageBox::warning(this, "Illegal entry", 
-			       "Empty user name or password"
-			       );
-	  return;
-	}
+        {
+          QMessageBox::warning(this, "Illegal entry", 
+                               "Empty user name or password"
+                               );
+          return;
+        }
       break;
 
-    case 1:
+    case 1: /* register tab */
       if (ui->lineEditUser2->text().length() > 0 &&
-	  ui->lineEditPw2->text().length() > 0)
-	{
-	  if (ui->lineEditPw2->text() !=
-	      ui->lineEditPwRepeat2->text())
-	    {
-	      QMessageBox::warning(this, "Password not repeated",
-				   "Need to repeat the same password!");
-	      return;
-	    }
-	  m_userdata = new UserData(ui->lineEditUser2->text(),
-				  ui->lineEditPw2->text(),
-				  true);
-	  QDialog::accept();
-	}
+          ui->lineEditPw2->text().length() > 0)
+        {
+          if (ui->lineEditPw2->text() !=
+              ui->lineEditPwRepeat2->text())
+            {
+              QMessageBox::warning(this, "Password not repeated",
+                                   "Need to repeat the same password!");
+              return;
+            }
+          m_userdata = new UserData(ui->lineEditUser2->text(),
+                                  ui->lineEditPw2->text(),
+                                    "",
+                                  true);
+          ui->tabScorchedEarth->setEnabled(true);
+          QDialog::accept();
+        }
       else
-	{
-	  QMessageBox::warning(this, 
-			       "Illegal entry",
-			       "Empty user name or password");
-	  return;
-	}
+        {
+          QMessageBox::warning(this, 
+                               "Illegal entry",
+                               "Empty user name or password");
+          return;
+        }
       break;
-					 
+
+    case 2: /* scorched earth tab */
+      if (ui->lineEditPw3->text().length() > 0 &&
+          ui->lineEditPw3Repeat->text().length() > 0)
+        {
+          if (ui->lineEditPw3->text() !=
+              ui->lineEditPw3Repeat->text())
+            {
+              QMessageBox::warning(this, "Password not repeated",
+                                   "Need to repeat the same password!");
+              return;
+            }
+          m_userdata = new UserData("",
+                                    "",
+                                    ui->lineEditPw3->text(),
+                                  true);
+          QDialog::accept();
+        }
+      else
+        {
+          QMessageBox::warning(this, 
+                               "Illegal entry",
+                               "Empty user name or password");
+          return;
+        }
+      break; /* Just to make the compiler happy. */
     default:
       QMessageBox::warning(this, "Dialog problem",
-			       "Tab not set upon exit");
+                               "Tab not set upon exit");
     }
 
   QDialog::accept();
--- userdata.h
+++ userdata.h
@@ -8,10 +8,13 @@ class UserData
 public:
   QString m_user;
   QString m_password;
+  QString m_scorchedearthpassword;
   bool    m_new;
 
-  UserData (QString user, QString password, const bool isnew) : 
-  m_user(user), m_password(password), m_new(isnew) {} 
+  UserData (QString user, QString password, 
+	    QString scorchedearthpw, const bool isnew) : 
+  m_user(user), m_password(password), 
+    m_scorchedearthpassword(scorchedearthpw), m_new(isnew) {} 
   ~UserData () {}
 };
 
